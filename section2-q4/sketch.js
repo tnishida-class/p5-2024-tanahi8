f7663e5ee3f263457bac4dac255e2aa8da798599
tanahi8 <2370591h@stu.kobe-u.ac.jp>
2024-10-22 10:53:49 +0900

    Update sketch.js
    スウェーデン国旗

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,17 @@
 // ギリシャ国旗
 function setup() {
-  const blue = color(0, 51, 160);
+  const yellow = color(255,255,0);
   createCanvas(270, 180);
-  // noStroke();
-  background(255);
+  noStroke();
+  background(0,185,255);
 
-  let d = height / 9; // 縞1本の太さ
+  
+  
 
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
-
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
-
-  fill(255);
+  fill(yellow);
+  rect(0,75,270,30);
+  
+  fill(yellow);
+  rect(90,0,30,180)
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
